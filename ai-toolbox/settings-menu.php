bda50cbbd9cb9c04ee9c9e42880f17a223606513
saledor <info@saledor.com>
2023-12-26 11:11:22 +0300

    validate api key when saving settings

--- ai-toolbox/settings-menu.php
+++ ai-toolbox/settings-menu.php
@@ -1,11 +1,33 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) exit;
 if (!defined('AI_TOOLBOX_INIT')) {
     exit; // Exit if accessed directly
 }
 
+function ai_toolbox_verify_api_key($api_key)
+{
+    $test_request = [
+        'headers' => [
+            'Authorization' => 'Bearer ' . $api_key
+        ],
+        'timeout' => 15
+    ];
+
+    $response = wp_remote_get("https://api.openai.com/v1/models", $test_request);
+
+    if (is_wp_error($response)) {
+        return false; // There was an error in the request
+    }
+
+    $response_code = wp_remote_retrieve_response_code($response);
+    return ($response_code >= 200 && $response_code < 300);
+}
+
+
 function ai_toolbox_settings_menu_page()
 {
+    $api_key_validity = false;
+
     // Check for POST request and nonce verification
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // Verify the nonce field
@@ -16,6 +38,20 @@ function ai_toolbox_settings_menu_page()
         $valid_versions = array('gpt-3.5-turbo-16k', 'gpt-4');
         $sanitized_version = sanitize_text_field($_POST['chatgpt_version']);
         $sanitized_key = sanitize_text_field($_POST['chatgpt_api_key']);
+
+        $api_key_validity = ai_toolbox_verify_api_key($sanitized_key);
+
+        // Verify the API key
+        if ($api_key_validity) {
+            update_option('ai_toolbox_chatgpt_api_key', $sanitized_key);
+            // Add a success message
+            echo '<div class="updated"><p>API Key verified and saved successfully!</p></div>';
+        } else {
+            // Add an error message
+            echo '<div class="error"><p>Invalid API Key. Please check and try again.</p></div>';
+            // Optionally, clear the invalid API key from options
+            update_option('ai_toolbox_chatgpt_api_key', '');
+        }
 
         // Check if the submitted version is in the array of valid versions
         if (in_array($sanitized_version, $valid_versions)) {
@@ -34,15 +70,21 @@ function ai_toolbox_settings_menu_page()
 
     <div class="container">
         <h3 class="my-3">AI ToolBox Settings</h3>
-        <!-- Information box for when API key is set -->
+        <!-- Information box based on API key validity -->
         <?php if (!empty($chatgpt_api_key)) : ?>
-            <div class="alert alert-info" role="alert">
-                Your API key is set. You can now add a new page or post to use the plugin:
-                <ul>
-                <li><a href="<?php echo esc_url(admin_url('post-new.php')); ?>">Add New Post</a></li>
-                <li><a href="<?php echo esc_url(admin_url('post-new.php?post_type=page')); ?>">Add New Page</a></li>
-                </ul>
-            </div>
+            <?php if ($api_key_validity) : ?>
+                <div class="alert alert-info" role="alert">
+                    Your API key is set. You can now add a new page or post to use the plugin:
+                    <ul>
+                        <li><a href="<?php echo esc_url(admin_url('post-new.php')); ?>">Add New Post</a></li>
+                        <li><a href="<?php echo esc_url(admin_url('post-new.php?post_type=page')); ?>">Add New Page</a></li>
+                    </ul>
+                </div>
+            <?php else : ?>
+                <div class="alert alert-warning" role="alert">
+                    Warning: Your API key is set but seems to be invalid. Couldn't retrieve models from OpenAI.
+                </div>
+            <?php endif; ?>
         <?php else : ?>
             <div class="alert alert-warning" role="alert">
                 Warning: API key is missing. Please get your API key from <a href="https://platform.openai.com/account/api-keys" target="_blank" rel="noopener noreferrer">OpenAI</a>.<br>
